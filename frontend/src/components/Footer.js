ec21468c564409483b0aafa1bc66696af0942bf8
Vasya-556 <vkobrin556@gmail.com>
2024-05-09 10:38:51 +0300

    Add working footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,22 @@
+import React, { useState } from 'react';
+import './Style.css';
+
 function Footer(){
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleFooter = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <div>
-
+            <button onClick={toggleFooter} className={`footer-toggle-btn ${isOpen ? 'footer-open' : ''}`}>
+                {isOpen ? 'Hide' : 'Show'} Footer
+            </button>
+            <div className={`footer-content ${isOpen ? 'footer-open' : ''}`}>
+                {/* Content of the footer */}
+                Footer Content
+            </div>
         </div>
     );
 }
